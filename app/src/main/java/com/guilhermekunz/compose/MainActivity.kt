9230a109829197b5e9bc9429a0145a84454b2044
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2021-08-02 17:33:04 -0300

    Rows, Columns & Basic Sizing

--- app/src/main/java/com/guilhermekunz/compose/MainActivity.kt
+++ app/src/main/java/com/guilhermekunz/compose/MainActivity.kt
@@ -3,36 +3,35 @@ package com.guilhermekunz.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.guilhermekunz.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            Row(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(300.dp)
+                    .background(Color.Green),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Guilherme")
+                Text("Butzen")
+                Text("Kunz")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTheme {
-        Greeting("Android")
     }
 }
