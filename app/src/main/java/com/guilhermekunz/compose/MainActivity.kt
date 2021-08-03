6517a4f12204e91a31a0bd5e5ca30bd17f9b1a5e
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2021-08-03 11:53:05 -0300

    Modifiers

--- app/src/main/java/com/guilhermekunz/compose/MainActivity.kt
+++ app/src/main/java/com/guilhermekunz/compose/MainActivity.kt
@@ -4,33 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.guilhermekunz.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
+            Column(
                 modifier = Modifier
-                    .width(200.dp)
-                    .height(300.dp)
-                    .background(Color.Green),
-                horizontalArrangement = Arrangement.SpaceAround,
-                verticalAlignment = Alignment.CenterVertically
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Red)
+                    .padding(10.dp)
+//                    .requiredWidth(600.dp)
             ) {
-                Text("Guilherme")
-                Text("Butzen")
-                Text("Kunz")
+                Text("Hello")
+                Spacer(modifier = Modifier.height(50.dp))
+                Text("World")
             }
         }
     }
